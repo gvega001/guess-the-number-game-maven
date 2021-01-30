f1db755ff26ee2b79a11975caba17b0444c653c5
Gabriel Vega <gvega001@gmail.com>
2021-01-29 19:52:14 -0800

    Implemented NumberGenerator with private fields and methods.

--- core/src/main/java/online/dev4you/NumberGeneratorImpl.java
+++ core/src/main/java/online/dev4you/NumberGeneratorImpl.java
@@ -1,4 +1,22 @@
 package online.dev4you;
 
-public class NumberGeneratorImpl {
+import java.util.Random;
+
+public class NumberGeneratorImpl implements NumberGenerator{
+
+    //******====  Fields     =====*****
+    private final Random random = new Random();
+
+    private int maxNumber = 100;
+
+    //******====  Public Methods =====****
+    @Override
+    public int next() {
+        return random.nextInt(maxNumber);
+    }
+
+    @Override
+    public int getMaxNumber() {
+        return maxNumber;
+    }
 }
