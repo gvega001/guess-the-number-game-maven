89164560d0ce1ea32c68caf466737ae5f592014d
Gabriel Vega <gvega001@gmail.com>
2021-01-29 21:45:25 -0800

    logger info to print number to console.

--- core/src/main/java/online/dev4you/Main.java
+++ core/src/main/java/online/dev4you/Main.java
@@ -1,24 +1,29 @@
 package online.dev4you;
 
-import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    private static final ILoggerFactory log = LoggerFactory.getILoggerFactory();
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     private static final String CONFIG_LOCACTION = "beans.xml";
 
     public static void main(String[] args) {
-        log.getLogger("Guess The Number Game");
+        log.info("Guess The Number Game");
+        ConfigurableApplicationContext context
+                = new ClassPathXmlApplicationContext(CONFIG_LOCACTION);
+
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+
+        //call the method next() to get a random number
+        int number = numberGenerator.next();
+
+        //log generated number
+        log.info("number = {}", number);
     }
 
-    ConfigurableApplicationContext context
-            = new ClassPathXmlApplicationContext(CONFIG_LOCACTION);
-
-    NumberGenerator numberGenerator
-            = context.getBean("numberGenerator", NumberGenerator.class);
 }
