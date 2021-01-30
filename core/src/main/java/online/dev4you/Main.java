75f018346834959ec340bd01f5779b3e894a836b
Gabriel Vega <gvega001@gmail.com>
2021-01-29 21:18:45 -0800

    created bean only one pojo

--- core/src/main/java/online/dev4you/Main.java
+++ core/src/main/java/online/dev4you/Main.java
@@ -2,12 +2,23 @@ package online.dev4you;
 
 import org.slf4j.ILoggerFactory;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final ILoggerFactory log = LoggerFactory.getILoggerFactory();
 
+    private static final String CONFIG_LOCACTION = "beans.xml";
+
     public static void main(String[] args) {
         log.getLogger("Guess The Number Game");
     }
+
+    ConfigurableApplicationContext context
+            = new ClassPathXmlApplicationContext(CONFIG_LOCACTION);
+
+    NumberGenerator numberGenerator
+            = context.getBean("numberGenerator", NumberGenerator.class);
 }
