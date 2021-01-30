20244870746acbed9a50e5b60950dd0a491fa036
Gabriel Vega <gvega001@gmail.com>
2021-01-29 22:29:19 -0800

    fields and methods for game interface

--- core/src/main/java/online/dev4you/Game.java
+++ core/src/main/java/online/dev4you/Game.java
@@ -1,4 +1,26 @@
 package online.dev4you;
 
 public interface Game {
+
+    int getNumber();
+
+    int getGuess();
+
+    void setGuess(int guess);
+
+    int getSmallest();
+
+    int getBiggest();
+
+    int getRemainGuesses();
+
+    void reset();
+
+    void check();
+
+    boolean isValidNumber();
+
+    boolean isGameWon();
+
+    boolean isGameLost();
 }
