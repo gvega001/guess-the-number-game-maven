f0e4393cd81604cf8c260671f532e74c280dda9a
Gabriel Vega <gvega001@gmail.com>
2021-02-03 19:04:31 -0800

    Using Autowired annotation. Deleted the bean for injecting the dependency of numberGenerator

--- core/src/main/java/online/dev4you/GameImpl.java
+++ core/src/main/java/online/dev4you/GameImpl.java
@@ -2,6 +2,7 @@ package online.dev4you;
 
 import  org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,6 +13,7 @@ public class GameImpl implements Game{
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     //**=== fields ***===
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 5;
     private int number;
@@ -38,11 +40,7 @@ public class GameImpl implements Game{
     }
 
     //**=== public methods ==
-    public void setNumberGenerator(NumberGenerator numberGenerator){
-        this.numberGenerator = numberGenerator;
-    }
-
-    @Override
+     @Override
     public int getNumber() {
         return number;
     }
