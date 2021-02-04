b6d7f7695b1cef0cacc39c2fb6323770213517d7
Gabriel Vega <gvega001@gmail.com>
2021-02-03 19:54:35 -0800

    Using component-scan with a base package.

--- core/src/main/java/online/dev4you/NumberGeneratorImpl.java
+++ core/src/main/java/online/dev4you/NumberGeneratorImpl.java
@@ -1,7 +1,10 @@
 package online.dev4you;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+@Component("generator")
 public class NumberGeneratorImpl implements NumberGenerator{
 
     //******====  Fields     =====*****
