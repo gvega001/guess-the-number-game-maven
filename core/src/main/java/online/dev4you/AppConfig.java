4fb99c74edcd8d82a149c14c7941174907b667d6
Gabriel Vega <gvega001@gmail.com>
2021-02-04 08:52:13 -0800

    AppConfig.java file created for bean creation on startup. Annotated @Configuration and @ComponentScan.

---
+++ core/src/main/java/online/dev4you/AppConfig.java
@@ -0,0 +1,9 @@
+package online.dev4you;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "online.dev4you")
+public class AppConfig {
+}
--- core/src/main/java/online/dev4you/Main.java
+++ core/src/main/java/online/dev4you/Main.java
@@ -3,18 +3,20 @@ package online.dev4you;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Scanner;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCACTION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
         ConfigurableApplicationContext context
-                = new ClassPathXmlApplicationContext(CONFIG_LOCACTION);
+                = new AnnotationConfigApplicationContext(AppConfig.class);
 
         //get the game bean context (container)
         NumberGenerator numberGenerator
