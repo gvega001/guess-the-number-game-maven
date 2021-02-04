dc367f55c140033554f8ffd9dba4df3cb746340b
Gabriel Vega <gvega001@gmail.com>
2021-02-04 15:50:41 -0800

    created bean for MessageGeneratorImpl

--- core/src/main/java/online/dev4you/Main.java
+++ core/src/main/java/online/dev4you/Main.java
@@ -18,27 +18,6 @@ public class Main {
         ConfigurableApplicationContext context
                 = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        //get the game bean context (container)
-        NumberGenerator numberGenerator
-                = context.getBean(NumberGenerator.class);
-
-        //call the method next() to get a random number
-        int number = numberGenerator.next();
-
-        //log generated number
-        log.info("number = {}", number);
-
-        //get the game bean context (container)
-        Game game =
-                context.getBean(Game.class);
-
-        MessageGenerator messageGenerator =
-                context.getBean(MessageGenerator.class);
-
-        log.info(messageGenerator.getMainMessage());
-        log.info(messageGenerator.getResultMessage());
-
-        //game.reset();
         context.close();
     }
 }
--- core/src/main/java/online/dev4you/MessageGeneratorImpl.java
+++ core/src/main/java/online/dev4you/MessageGeneratorImpl.java
@@ -19,17 +19,39 @@ public class MessageGeneratorImpl implements MessageGenerator{
     private Game game;
     private int guessCount =10;
 
+
     // === init ==
     @PostConstruct
-    @Override
-    public String getMainMessage() {
-          return "getMainMessage()called";
+    public void init(){
+        log.info("game ={}", game);
     }
 
-    @PreDestroy
+    @Override
+    public String getMainMessage() {
+          return "Number is between " +
+                  game.getSmallest() +
+                  " and "+
+                  game.getBiggest() +
+                  ". Can you guess it?";
+    }
+
     @Override
     public String getResultMessage() {
-        return "getResultMessage() called";
+        if(game.isGameWon()){
+            return "You guessed it! The number was " + game.getNumber();
+        } else  if (game.isGameLost()){
+            return "You lost. The number was " + game.getNumber();
+        } else  if (!game.isValidNumberRange()){
+            return "Invalid number range!";
+        } else if (game.getRemainGuesses() == guessCount){
+            return "What is your first guess";
+        } else {
+            String direction ="Lower";
+            if(game.getGuess() <game.getNumber()){
+                direction = "Higher";
+            }
+            return direction + "! You have " + game.getRemainGuesses() + " guess left";
+        }
     }
 
 }
